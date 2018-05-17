b817d0b0b99b974571eede4f0c7aa7526523fd5e
Paul Siaro <siarohau@gmail.com>
2018-05-17 04:47:58 +0300

    add sync st3 two packages

--- sync_dropbox.cmd
+++ sync_dropbox.cmd
@@ -5,9 +5,19 @@ Echo "Sync files with DROPBOX for win10"
 Echo "shortcut "mv" not working"
 
 
+
 Echo "Sync Sublime Text 3"
 move "%AppData%\Sublime Text 3\Packages\User" %USERPROFILE%\Dropbox\work\ST3
 mklink /D "%AppData%\Sublime Text 3\Packages\User" %USERPROFILE%\Dropbox\work\ST3\User
+
+Echo "Sync Sublime Text 3 - Package console-log"
+move "%AppData%\Sublime Text 3\Packages\console-log" %USERPROFILE%\Dropbox\work\ST3
+mklink /D "%AppData%\Sublime Text 3\Packages\console-log" %USERPROFILE%\Dropbox\work\ST3\console-log
+
+Echo "Sync Sublime Text 3 - Package Open-Include"
+move "%AppData%\Sublime Text 3\Packages\Open-Include" %USERPROFILE%\Dropbox\work\ST3
+mklink /D "%AppData%\Sublime Text 3\Packages\Open-Include" %USERPROFILE%\Dropbox\work\ST3\Open-Include
+
 
 Echo "Sync FileZilla"
 move "%AppData%\FileZilla" %USERPROFILE%\Dropbox\work\FileZilla
