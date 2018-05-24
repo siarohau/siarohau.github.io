170d60d74b0af03f01dc3160fcdfcf88ea4146ac
Paul Siaro <siarohau@gmail.com>
2018-05-24 13:12:16 +0300

    add sublime packages (BufferScroll-st3, Tag-st3)

--- sync_dropbox.cmd
+++ sync_dropbox.cmd
@@ -18,6 +18,14 @@ Echo "Sync Sublime Text 3 - Package Open-Include"
 move "%AppData%\Sublime Text 3\Packages\Open-Include" %USERPROFILE%\Dropbox\work\ST3
 mklink /D "%AppData%\Sublime Text 3\Packages\Open-Include" %USERPROFILE%\Dropbox\work\ST3\Open-Include
 
+Echo "Sync Sublime Text 3 - Package BufferScroll-st3"
+move "%AppData%\Sublime Text 3\Packages\BufferScroll-st3" %USERPROFILE%\Dropbox\work\ST3
+mklink /D "%AppData%\Sublime Text 3\Packages\BufferScroll-st3" %USERPROFILE%\Dropbox\work\ST3\BufferScroll-st3
+
+Echo "Sync Sublime Text 3 - Package Tag-st3"
+move "%AppData%\Sublime Text 3\Packages\Tag-st3" %USERPROFILE%\Dropbox\work\ST3
+mklink /D "%AppData%\Sublime Text 3\Packages\Tag-st3" %USERPROFILE%\Dropbox\work\ST3\Tag-st3
+
 
 Echo "Sync FileZilla"
 move "%AppData%\FileZilla" %USERPROFILE%\Dropbox\work\FileZilla
